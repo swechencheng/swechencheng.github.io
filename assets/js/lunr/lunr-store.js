cb7c22e3f074fee1bc90f3d465f84b8cd6cfd48c
swechencheng <swechencheng@users.noreply.github.com>
2025-07-29 14:10:18 +0000

    deploy: 39f864995d275d32c188a4b4b49a7790452ba9b9

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"This is my first Hello World! blog using Jekyll. It is based on Mimimal Mistakes GitHub Pages site starter. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can...","categories": ["blog"],
+        "tags": ["update"],
+        "url": "/blog/hello-world/",
+        "teaser": null
+      },{
+        "title": "Remote control a power supply",
+        "excerpt":"When working with embedded devices, I need to control, e.g. voltage and current on some test point. I got a good power supply Aim-TTi MX100TP, which I can have very accurate control over three outputs. Here is the manual for MX100TP. Moreover, I found out that MX100TP has a remote...","categories": ["blog"],
+        "tags": ["hardware","power supply","serial port","Linux","Python"],
+        "url": "/blog/remote-control-a-power-supply/",
+        "teaser": null
+      },{
+        "title": "Custom `upperdir` in meta-readonly-rootfs-overlay",
+        "excerpt":"I have been working and designing a new Embedded Linux with a brand new upgrade mechanism on an 7-10 year-old legacy Embedded Linux devices which had no atomic rootfs upgrade mechanism. the legacy system has a proprietary package manager which is working fine and I have to keep the compatibility...","categories": ["blog"],
+        "tags": ["Embedded Linux","Yocto","meta-readonly-rootfs-overlay"],
+        "url": "/blog/custom-upperdir-in-meta-readonly-rootfs-overlay/",
+        "teaser": null
+      },{
+        "title": "Yocto custom UBI image guide in detail",
+        "excerpt":"In this blog, I will discuss something new I found based on my previous blog, as well as another Marcus Folkesson’s blog. I strongly recommend you to go through Marcus Folkesson’s blog first, so that you get a basic idea how to integrate multiple volumes in Yocto for a UBI...","categories": ["blog"],
+        "tags": ["Embedded Linux","Yocto","NAND","UBI","squashfs","NXP","i.MX"],
+        "url": "/blog/yocto-custom-ubinize/",
+        "teaser": null
+      },{
+        "title": "Free Gemini 2.5 Pro Investment Opportunity Analysis",
+        "excerpt":"DISCLAIMER This blog is for demonstration purposes only and does not constitute investment advice. Always conduct your own research or consult a qualified financial advisor before making any investment decisions. Let me begin with this video. It is a demo of using Gemini 2.5 Pro Deep Research for free, which...","categories": ["blog"],
+        "tags": ["AI","Gemini","Investment","Prompt Engineering","Deep Research","OMXS30"],
+        "url": "/blog/free-gemini-financial-advice/",
+        "teaser": null
+      },{
+        "title": "Super cheap: A Raspberry Pi VPN router for IoT devices",
+        "excerpt":"DISCLAIMER Educational &amp; Hobby Use Only The content on this blog is provided solely for educational, informational, and hobby purposes. I have no intention of infringing upon or circumventing any manufacturer’s terms of service, license agreements, or intellectual property rights. No Warranty All information is provided “as is,” without warranty...","categories": ["blog"],
+        "tags": ["Embedded Linux","Raspberry Pi","Networking","Geofence","Tailscale","VPN","robot vacuum cleaner","IoT device"],
+        "url": "/blog/rpi-vpn-router/",
+        "teaser": null
+      }]
